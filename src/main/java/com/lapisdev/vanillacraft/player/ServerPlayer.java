52b7822a88bfddaf5963426e27f4d4d9a1807f91
dylancode <opensource@dylancode.dev>
2026-05-07 21:12:13 +0100

    create ServerPlayer

---
+++ src/main/java/com/lapisdev/vanillacraft/player/ServerPlayer.java
@@ -0,0 +1,46 @@
+package com.lapisdev.vanillacraft.player;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import static com.lapisdev.vanillacraft.database.Query.sqlSelect;
+import static com.lapisdev.vanillacraft.database.Query.sqlUpdateOrInsert;
+
+public class ServerPlayer {
+    public int id;
+    public String discordUuid;
+    public UUID minecraftUuid;
+
+    public static ServerPlayer fromId(int playerId) {
+        return fromResultSet(sqlSelect("select * from player where player_id = ?", playerId));
+    }
+
+    public static ServerPlayer fromMinecraftUuid(UUID minecraftUuid) {
+        return fromResultSet(sqlSelect("select * from player where minecraft_uuid = ?", minecraftUuid.toString()));
+    }
+
+    public static ServerPlayer fromDiscordUuid(String discordUuid) {
+        return fromResultSet(sqlSelect("select * from player where discord_uuid = ?", discordUuid));
+    }
+
+    public void save() {
+        sqlUpdateOrInsert("update player set discord_uuid = ?, minecraft_uuid = ? where player_id = ?",
+                "insert into player (discord_uuid, minecraft_uuid) values (?, ?)",
+                discordUuid, minecraftUuid.toString(), id);
+        // todo: save `id` from key
+    }
+
+    private static ServerPlayer fromResultSet(ResultSet rs) {
+        try {
+            if (!rs.next()) return null;
+            ServerPlayer player = new ServerPlayer();
+            player.id = rs.getInt("player_id");
+            player.discordUuid = rs.getString("discord_uuid");
+            player.minecraftUuid = UUID.fromString(rs.getString("minecraft_uuid"));
+            return player;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
