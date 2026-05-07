fb15eec6273ed35b090c22f8f014497c5651afe3
dylancode <opensource@dylancode.dev>
2026-05-07 10:26:22 +0100

    initial Vanillacraft Season 3 plugin

---
+++ src/main/java/com/lapisdev/vanillacraft/VanillacraftPlugin.java
@@ -0,0 +1,16 @@
+package com.lapisdev.vanillacraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VanillacraftPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
