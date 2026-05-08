5a90cc493409890f7da35c4c7509f8246ed94c9e
dylancode <opensource@dylancode.dev>
2026-05-08 15:01:24 +0100

    fix bug in JoinListener for whitelist

--- src/main/java/com/lapisdev/vanillacraft/whitelist/JoinListener.java
+++ src/main/java/com/lapisdev/vanillacraft/whitelist/JoinListener.java
@@ -3,6 +3,7 @@ package com.lapisdev.vanillacraft.whitelist;
 import com.lapisdev.vanillacraft.kick.Kick;
 import com.lapisdev.vanillacraft.player.ServerPlayer;
 import com.lapisdev.vanillacraft.region.Region;
+import com.lapisdev.vanillacraft.task.RunTask;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,10 +17,12 @@ public class JoinListener implements Listener {
     public void onJoin(PlayerJoinEvent e) {
         Player minecraftPlayer = e.getPlayer();
         UUID minecraftUuid = minecraftPlayer.getUniqueId();
-        ServerPlayer player = new ServerPlayer();
-        player.minecraftUuid = minecraftUuid;
+        ServerPlayer player = ServerPlayer.fromMinecraftUuid(minecraftUuid);
         if (player == null) {
-            new Kick(player, Component.text("You haven't been whitelisted, or haven't linked your account.\nPlease link using the #get-whitelisted channel on discord!"));
+            ServerPlayer loggedInPlayer = new ServerPlayer();
+            loggedInPlayer.minecraftUuid = minecraftUuid;
+            Kick kick = new Kick(loggedInPlayer, Component.text("You haven't been whitelisted, or haven't linked your account.\nPlease link using the #get-whitelisted channel on discord!"));
+            RunTask.sync(kick::execute);
         }
         // todo: get player's region and check it exists
     }
