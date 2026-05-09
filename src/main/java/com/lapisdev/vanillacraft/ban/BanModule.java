144e460ae60ffbb9657bc816db19854bc1ddc9fa
dylancode <opensource@dylancode.dev>
2026-05-09 13:07:23 +0100

    link events and commands in BanModule

--- src/main/java/com/lapisdev/vanillacraft/ban/BanModule.java
+++ src/main/java/com/lapisdev/vanillacraft/ban/BanModule.java
@@ -1,8 +1,15 @@
 package com.lapisdev.vanillacraft.ban;
 
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+
+import static com.lapisdev.vanillacraft.VanillacraftPlugin.handle;
+import static com.lapisdev.vanillacraft.VanillacraftPlugin.plugin;
 
 public class BanModule {
     public BanModule() {
-
+        plugin().getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, (registry) -> {
+            BanCmdMc.register(registry.registrar());
+        });
+        handle(new BanJoinListener());
     }
 }
