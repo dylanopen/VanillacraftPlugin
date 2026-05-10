28bc1ed39ec7bdc92895e0c10b3c1d4c43000ddf
dylancode <opensource@dylancode.dev>
2026-05-10 13:31:20 +0100

    create DiscordCommands.staffCmd to reduce boilerplate

--- src/main/java/com/lapisdev/vanillacraft/discord/DiscordCommands.java
+++ src/main/java/com/lapisdev/vanillacraft/discord/DiscordCommands.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
 import static com.lapisdev.vanillacraft.discord.Discord.jda;
 
@@ -13,36 +14,26 @@ public class DiscordCommands {
     public DiscordCommands() {
         jda.updateCommands().addCommands(
 
-                Commands.slash("ban", "Ban a user from the Minecraft server, and prevent them from viewing discord channels.")
+                staffCmd(Commands.slash("ban", "Ban a user from the Minecraft server, and prevent them from viewing discord channels.")
                         .addOptions(new OptionData(OptionType.USER, "user", "Ban a player's account associated with this discord account."))
                         .addOptions(new OptionData(OptionType.STRING, "reason", "The reason to provide for the ban."))
-                        .addOptions(new OptionData(OptionType.STRING, "duration", "The duration of the ban (e.g. `1d`, `2h30m`, `45m`, etc.), else permanent."))
-                        .setContexts(InteractionContextType.GUILD)
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MESSAGE_MANAGE)),
+                        .addOptions(new OptionData(OptionType.STRING, "duration", "The duration of the ban (e.g. `1d`, `2h30m`, `45m`, etc.), else permanent."))),
 
-                Commands.slash("mckick", "Kick a player from the Minecraft server, given their Minecraft username or a Discord ping.")
+                staffCmd(Commands.slash("mckick", "Kick a player from the Minecraft server, given their Minecraft username or a Discord ping.")
                         .addOptions(new OptionData(OptionType.USER, "dcuser", "Kick a player's Minecraft account associated with this discord account."))
                         .addOptions(new OptionData(OptionType.STRING, "mcuser", "Kick the Minecraft account with the given username."))
-                        .addOptions(new OptionData(OptionType.STRING, "reason", "The reason to provide for the kick."))
-                        .setContexts(InteractionContextType.GUILD)
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MESSAGE_MANAGE)),
+                        .addOptions(new OptionData(OptionType.STRING, "reason", "The reason to provide for the kick."))),
 
-                Commands.slash("mcrestart", "Restart the Minecraft server, only to be used if something is wrong and it needs a restart.")
-                        .setContexts(InteractionContextType.GUILD)
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MESSAGE_MANAGE)),
+                staffCmd(Commands.slash("mcrestart", "Restart the Minecraft server, only to be used if something is wrong and it needs a restart.")),
 
-                Commands.slash("playerlist", "Get a list of all players currently online on the Minecraft server.")
-                        .setContexts(InteractionContextType.GUILD)
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.VIEW_CHANNEL)),
+                staffCmd(Commands.slash("maintenance-on", "Put the Minecraft server under maintenance, kicking all non-staff members after 10 seconds.")),
 
-                Commands.slash("maintenance-on", "Put the Minecraft server under maintenance, kicking all non-staff members after 10 seconds.")
-                        .setContexts(InteractionContextType.GUILD)
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MESSAGE_MANAGE)),
-
-                Commands.slash("maintenance-off", "Take the Minecraft server out of maintenance mode, allowing all members to join again.")
-                        .setContexts(InteractionContextType.GUILD)
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MESSAGE_MANAGE))
-
+                staffCmd(Commands.slash("maintenance-off", "Take the Minecraft server out of maintenance mode, allowing all members to join again."))
         ).queue();
+    }
+
+    private SlashCommandData staffCmd(SlashCommandData cmd) {
+        return cmd.setContexts(InteractionContextType.GUILD)
+                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MESSAGE_MANAGE));
     }
 }
